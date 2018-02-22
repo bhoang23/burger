35fcc6654ee4e9f97fa964c851d0f4f8da6abef7
bhoang23 <bhoang02@gmail.com>
2018-02-22 14:38:20 -0600

    added files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burger_name) VALUES ('Big Mac');
+INSERT INTO burgers(burger_name) VALUES ('Patty Melt');
+INSERT INTO burgers(burger_name) VALUES ('Whopper');
+INSERT INTO burgers(burger_name) VALUES ('Bacon Cheese Burger');
+INSERT INTO burgers(burger_name) VALUES ('Tofu Burger');
